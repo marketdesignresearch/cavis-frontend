8dc8ac1ba54adcab49c382b2b6780725cf4ab39e
fabio <islerfab@gmail.com>
2019-08-14 12:54:02 +0400

    feat(introjs): introduced introjs with example

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,17 @@
 <template>
-  <div class="container">
-    Configured API: {{ apiUrl }}
-  </div>
+  <div class="container">Configured API: {{ apiUrl }}</div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from 'vue'
 import api from '@/services/api'
 
 export default Vue.extend({
   computed: {
     apiUrl(): string | undefined {
       return api().defaults.baseURL
-    },
+    }
   },
   name: 'home'
-});
+})
 </script>
